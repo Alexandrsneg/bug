de72317c5dbd3410d55ecc8930129675e10a3ccf
Alexandrsneg <ai.kps42@gmail.com>
2020-03-06 17:09:14 +0700

    без local.properties gradle

--- app/src/main/java/ru/sneg/android/bug/MainActivity.kt
+++ app/src/main/java/ru/sneg/android/bug/MainActivity.kt
@@ -2,11 +2,19 @@ package ru.sneg.android.bug
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+    val quantity = 10;
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+fun  main(args: Array<String>){
+    println("fafafa")
+}
+
 }
