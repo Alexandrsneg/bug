6c6d099de79e79ef66002d5f768c7e4b82e3bd73
Alexandrsneg <ai.kps42@gmail.com>
2020-03-08 17:48:01 +0700

    added Activity "blind", Activity "game mode" and "bug placement" with btns and Surface View

---
+++ app/src/main/java/ru/sneg/android/bug/blind.kt
@@ -0,0 +1,12 @@
+package ru.sneg.android.bug
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class blind : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_blind)
+    }
+}
---
+++ app/src/main/java/ru/sneg/android/bug/bugPlacementPlayer1.kt
@@ -0,0 +1,12 @@
+package ru.sneg.android.bug
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class bugPlacementPlayer1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bug_placement_player1)
+    }
+}
