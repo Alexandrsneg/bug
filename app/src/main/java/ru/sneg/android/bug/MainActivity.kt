d0d50733ea2cc144e1b341ccd674a75d75de9243
Alexandrsneg <ai.kps42@gmail.com>
2020-03-09 00:01:58 +0700

    hide the title bars, made up switching between MainActivity, LoginActivity and Sign Up activity

--- app/src/main/java/ru/sneg/android/bug/MainActivity.kt
+++ app/src/main/java/ru/sneg/android/bug/MainActivity.kt
@@ -3,24 +3,38 @@ package ru.sneg.android.bug
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.content.Intent
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.view.Window
+import android.view.WindowManager
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
-    val quantity = 10;
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN )
         setContentView(R.layout.activity_main)
+
     }
 
-fun  main(args: Array<String>){
-    var forr = 1..10
-    for (y in forr){
-        println(quantity)
+    fun signInBtn(view: View) {
+        val intent = Intent(this@MainActivity, LoginActivity::class.java)
+        startActivity(intent)
     }
 
-    var arr = arrayOf(1,2,3)
-    println(arr[0])
-}
+    fun signUpBtn(view: View) {
+        val intent = Intent(this@MainActivity, SignUp::class.java)
+        startActivity(intent)
+    }
+
 
 }
