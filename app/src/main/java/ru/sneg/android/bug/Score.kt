e20dd9b57292d94063d4b92b50e1497ea4b7b50c
Alexandrsneg <ai.kps42@gmail.com>
2020-03-08 12:07:04 +0700

    made up btns images, added Activity of profile, score, sign up, and created background images for it

---
+++ app/src/main/java/ru/sneg/android/bug/Score.kt
@@ -0,0 +1,12 @@
+package ru.sneg.android.bug
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Score : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_score)
+    }
+}
