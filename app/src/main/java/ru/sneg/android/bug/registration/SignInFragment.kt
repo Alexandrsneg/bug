3043e0c603338bf25252a63b39e12b35537ad18e
Alexandrsneg <ai.kps42@gmail.com>
2020-03-20 12:26:51 +0700

    уменьшил кнопки входа и регистрации, добавил подчеркнутую ссылку ведщую на фргамент регистрации

--- app/src/main/java/ru/sneg/android/bug/registration/SignInFragment.kt
+++ app/src/main/java/ru/sneg/android/bug/registration/SignInFragment.kt
@@ -2,16 +2,20 @@ package ru.sneg.android.bug.registration
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.*
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import com.arellomobile.mvp.MvpAppCompatFragment
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.arellomobile.mvp.presenter.ProvidePresenter
 import kotlinx.android.synthetic.main.fragment_sign_in.*
-import kotlinx.android.synthetic.main.fragment_sign_in.editText
-import kotlinx.android.synthetic.main.fragment_sign_in.editText2
 import ru.sneg.android.bug.ProfileActivity
 import ru.sneg.android.bug.R
+
 
 class SignInFragment : MvpAppCompatFragment(), ISignInView {
 
@@ -41,8 +45,9 @@ class SignInFragment : MvpAppCompatFragment(), ISignInView {
             presenter.signIn("${editText.text}", "${editText2.text}")
         }
 
+
          // обработчик нажатия кнопк регистрации во ФРАГМЕНТЕ, реализует переход на др. ФРАГМЕНТ
-        bSignUpBtn.setOnClickListener {
+        tvSignUpBtn.setOnClickListener {
             var fr = getFragmentManager()?.beginTransaction()
             fr?.replace(R.id.containerActivity, SignUpFragment())
             fr?.commit()
